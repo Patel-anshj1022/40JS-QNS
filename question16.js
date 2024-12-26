4dd9dc27609bd9d97638804390496cc6a3df05f2
Patel-anshj1022 <patelanshj950@gmail.com>
2024-12-26 11:19:46 +0300

    Answered Qn16

---
+++ question16.js
@@ -0,0 +1,7 @@
+//Question 16 : What is a function in JavaScript, and how do you define one?
+//Answer : A function in JavaScript is a block of reusable code that performs a specific task or calculates a value
+
+function functionName(parameter1, parameter2,) {
+    return result; 
+  }
+  
