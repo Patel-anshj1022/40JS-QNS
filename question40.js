3808adf3fc93de20d9409888e6acac2c05c644ea
Patel-anshj1022 <patelanshj950@gmail.com>
2024-12-26 11:26:03 +0300

    Answered Qn40

---
+++ question40.js
@@ -0,0 +1,6 @@
+//question 40 : What is the purpose of the break and continue keywords in loops?
+// Answer : break
+//Purpose: It immediately terminates the loop, regardless of whether the loop condition is satisfied.
+
+//continue
+//Purpose: It skips the current iteration of the loop and proceeds to the next iteration.
