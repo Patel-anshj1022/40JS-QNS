489590e412eff6de4330d65c2d3976c1d12493d3
Patel-anshj1022 <patelanshj950@gmail.com>
2024-12-26 11:25:57 +0300

    Answered Qn39

---
+++ question39.js
@@ -0,0 +1,9 @@
+//Question 39 : Write a for...of loop to iterate over the following array:
+//Answer : const fruits = ["apple", "banana", "cherry"];
+
+
+const fruits = ["apple", "banana", "cherry"];
+
+for (const fruit of fruits) {
+  console.log(fruit);
+}
