644f997a2ff74453ab000845f305b140f99993db
Patel-anshj1022 <patelanshj950@gmail.com>
2024-12-26 11:16:53 +0300

    Answered Qn12

---
+++ question12.js
@@ -0,0 +1,5 @@
+//Question 12 : What will the following code output, and why?
+//console.log(2 + "2");
+//console.log(2 - "2");
+//Answer : (22): The + operator in JavaScript is overloaded. When one operand is a string, it triggers string concatenation.
+//Answer : (0): The - operator is strictly for numeric subtraction. When one operand is not a number, JavaScript tries to coerce the operand to a number.
