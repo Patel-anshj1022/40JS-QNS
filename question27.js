99206262f6fc4ddcf0e018fbab7d0b36ff6ecdde
Patel-anshj1022 <patelanshj950@gmail.com>
2024-12-26 11:22:47 +0300

    Answered Qn27

---
+++ question27.js
@@ -0,0 +1,6 @@
+//Question 27 : What is the difference between innerHTML and textContent?
+//Answer : innerHTML
+//Purpose: Retrieves or sets the HTML content inside an element, including any tags.
+
+//textContent
+//Purpose: Retrieves or sets only the text content of an element, ignoring any HTML tags.
