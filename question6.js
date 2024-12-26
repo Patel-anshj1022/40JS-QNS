5407f0817fb8a66ec04a48773316fa57d2c2da93
Patel-anshj1022 <patelanshj950@gmail.com>
2024-12-26 11:15:20 +0300

    Answered Qn6

---
+++ question6.js
@@ -0,0 +1,14 @@
+//Question 6 : What are data types in JavaScript? List at least three examples.
+//Answers : Data types in JavaScript represent the types of values that can be assigned to variables. JavaScript has two categories of data types: Primitive Data Types and Non-Primitive (Reference) Data Types.
+// Primitive Data Types:
+
+//String: Example: "Hello, World!"
+//Number: Example: 42, 3.14
+//Boolean: Represents logical values, either true or false.
+
+
+//Non-Primitive Data Types:
+
+//Object: Example: { name: "Tilak", age: 25 }
+//Array: Example: [1, 2, 3, 4]
+//Function: Example: function greet() { return "Hello!"; }
