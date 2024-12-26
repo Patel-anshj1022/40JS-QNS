e3fc562da9e56b1bfb0ca82acefd8720ef4167a3
Patel-anshj1022 <patelanshj950@gmail.com>
2024-12-26 11:24:33 +0300

    Answered Qn34

---
+++ question34.js
@@ -0,0 +1,6 @@
+//Question 34 : How do you add a new key-value pair to an existing object?
+//Answer : Using Dot Notation:
+
+let obj = { name: 'tilak', age: 18 };
+obj.city = 'south africa'; 
+console.log(obj);
