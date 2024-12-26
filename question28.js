530784ae8c5dcbe08bb63a2082cd81d672656864
Patel-anshj1022 <patelanshj950@gmail.com>
2024-12-26 11:23:05 +0300

    Answered Qn28

---
+++ question28.js
@@ -0,0 +1,4 @@
+//Question 28 : How can you prevent a form from submitting when a user clicks a submit button?
+//Answer : Using the event.preventDefault() Method
+//You can listen for the submit event on the form and call
+// event.preventDefault() to stop the default behavior of submitting the form.
