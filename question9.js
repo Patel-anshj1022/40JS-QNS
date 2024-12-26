7c7c4f5d476a4a931166a1ea299fb12a1dd40356
Patel-anshj1022 <patelanshj950@gmail.com>
2024-12-26 11:16:02 +0300

    Answered Qn9

---
+++ question9.js
@@ -0,0 +1,2 @@
+//Question 9 : Explain what NaN means in JavaScript.
+//Answer : In JavaScript, NaN stands for "Not-a-Number". It is a special value that indicates that a value is not a valid number.
